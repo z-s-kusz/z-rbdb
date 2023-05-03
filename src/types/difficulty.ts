538512d393f331c1f48ea185e5e361029abfca9a
Zach Kusz <zack.kusz@capitollien.com>
2023-05-02 23:13:16 -0500

    add types

---
+++ src/types/difficulty.ts
@@ -0,0 +1,6 @@
+export default interface Difficulty {
+    vocals: number;
+    guitar: number;
+    bass: number;
+    drums: number;
+}
---
+++ src/types/song.ts
@@ -0,0 +1,17 @@
+import Difficulty from "./difficulty";
+
+export default interface Song {
+    title: string;
+    artist: string;
+    album: string;
+    year: number;
+    genres: string[];
+    primaryGenere: string;
+    owned: boolean;
+    source: 'RB4' | 'RB4:Rivals' | 'RB1' | 'RB2' | 'RB3' | 'RB:Lego' | 'RB:Greenday' | 'DLC';
+    owner: 'Zach' | 'Pat' | '';
+    thank: 'Tom' | 'Eric' | 'Maria' | 'Katy' | 'Laura' | 'Scooter' | 'Zach' | 'Pat' | 'Harmonix';
+    harmonies: boolean;
+    rbDifficulty: Difficulty;
+    votedDifficulty: Difficulty;
+}
