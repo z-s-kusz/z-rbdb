56d04331eee4e5bdd06587c341ca32923b17abb2
Zach Kusz <zack.kusz@capitollien.com>
2023-05-16 16:24:03 -0500

    feat: added search and sorting

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useGetOwnedSongs } from '@/hooks/useGetOwnedSongs';
-import SongCardSmall from './song-cards/SongCardSmall.vue';
+import SortableSongCardList from './song-cards/SortableSongCardList.vue';
 
 const { songs, isLoading, error, getSongs } = useGetOwnedSongs();
 </script>
@@ -13,9 +13,8 @@ const { songs, isLoading, error, getSongs } = useGetOwnedSongs();
             <v-divider thickness="4" color="blue-accent-1"></v-divider>
             <p>Search the RockBand Database by clicking explore at the top of the page.</p>
             <v-divider thickness="4" color="blue-accent-1"></v-divider>
-            <p>...or scroll, scroll, scroll through what we already have below.</p>
+            <p>...or search through what we already have below.</p>
         </section>
-        <p class="text-purple">* local search coming soon!</p>
     </v-card>
 
     <template v-if="error">
@@ -23,9 +22,9 @@ const { songs, isLoading, error, getSongs } = useGetOwnedSongs();
         <v-btn color="error" @Click="getSongs">Retry</v-btn>
     </template>
     <!-- TODO add loading state that WON'T flash on/off if loading happens fast, which it should -->
-    <section class="card-container" v-else>
-        <SongCardSmall v-for="song in songs" :song="song" />
-    </section>
+    <template v-else>
+        <SortableSongCardList :songs="songs" />
+    </template>
 </template>
 
 <style lang="scss" scoped>
@@ -46,9 +45,5 @@ const { songs, isLoading, error, getSongs } = useGetOwnedSongs();
     text-fill-color: transparent;
     -webkit-background-clip: text;
     background-clip: text;
-}
-
-.card-container {
-    padding: 0.5rem;
 }
 </style>
---
+++ src/components/song-cards/SortableSongCardList.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import Song from '@/types/song';
+import { computed, ref } from 'vue';
+import SongCardSmall from './SongCardSmall.vue';
+
+interface Props {
+    songs: Song[];
+}
+type SortType = 'artist' | 'title';
+
+const props = defineProps<Props>();
+const sortSwitch = ref<SortType>('title');
+const search = ref('');
+
+const artistClass = computed(() => {
+    if (sortSwitch.value === 'artist') return 'active';
+    return '';
+});
+const titleClass = computed(() => {
+    if (sortSwitch.value === 'title') return 'active';
+    return '';
+});
+
+const emptySearchMessage = computed<string>(() => {
+    const lowerCaseSearch = search.value ? search.value.toLowerCase().trim() : '';
+    if (lowerCaseSearch !== '' && sortedSongs.value.length === 0) {
+        return 'No results found.';
+    }
+    return '';
+});
+
+const sortedSongs = computed(() => {
+    let { songs } = props;
+
+    const lowerCaseSearch = search.value ? search.value.toLowerCase().trim() : '';
+    if (lowerCaseSearch !== '') {
+        songs = songs.filter((song) => {
+            const matchingSong = song.album.toLowerCase().includes(lowerCaseSearch) || song.artist.toLowerCase().includes(lowerCaseSearch)
+            return matchingSong
+        });
+    }
+
+    const sort = sortSwitch.value;
+    const ignoredChars = ['(', "'", '"'];
+    return songs.sort((a, b) => {
+        let aProp = a[sort];
+        let bProp = b[sort];
+        if (ignoredChars.includes(aProp.charAt(0))) aProp = aProp.substring(1, aProp.length - 1);
+        if (ignoredChars.includes(bProp.charAt(0))) bProp = bProp.substring(1, bProp.length - 1);
+
+        if (aProp < bProp) {
+            return -1;
+        }
+        if (aProp > bProp) {
+            return 1;
+        }
+        return 0;
+    });
+});
+
+</script>
+
+<template>
+    <section class="sort-container">
+        <div class="switch-container">
+            <span>Sort:&nbsp;</span>
+            <span :class="artistClass">Artist</span>
+            <v-switch v-model="sortSwitch" hide-details true-value="title" false-value="artist" class="switch"
+                density="compact"></v-switch>
+            <span :class="titleClass">Title</span>
+        </div>
+        <v-text-field v-model="search" variant="underlined" single-line density="compact" hide-details clearable
+            label="Search" class="search"></v-text-field>
+    </section>
+    <section class="card-container">
+        <SongCardSmall v-for="song in sortedSongs" :song="song" />
+    </section>
+    <v-card class="card-container error" v-show="emptySearchMessage">
+        {{ emptySearchMessage }}
+    </v-card>
+</template>
+
+<style lang="scss" scoped>
+.sort-container {
+    padding: 0 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.switch-container {
+    display: flex;
+    align-items: center;
+    flex: 1 1 50%;
+}
+
+.search {
+    flex: 0 1 50%;
+}
+
+.switch {
+    flex: 0 1 auto;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+}
+
+.active {
+    color: #FF9800;
+}
+
+.card-container {
+    padding: 0.5rem;
+}
+
+.error {
+    margin: 1rem;
+}
+</style>
