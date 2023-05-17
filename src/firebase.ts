1b99c3ac47a866955a212e185d666505050ba8f9
Zach Kusz <zack.kusz@capitollien.com>
2023-05-17 14:16:59 -0500

    fix: keep me logged in when credentials are still good

--- src/firebase.ts
+++ src/firebase.ts
@@ -23,10 +23,10 @@ const firebaseConfig = {
 
   // auth
   const globalUserRef = ref<User | null>(null);
+  if (auth.currentUser) globalUserRef.value = auth.currentUser; // set globalUserRef at start if user still valid
+
   onAuthStateChanged(auth, (user) => {
     if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
       globalUserRef.value = user;
     } else {
         // User is signed out
